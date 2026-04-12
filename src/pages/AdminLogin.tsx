511ef1b983cd8298cfb2c61fe5400eef04227c3a
AnatolyM <amisteke@gmail.com>
2026-04-13 02:22:28 +0300

    http://159.198.43.22:3000

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Lock } from "lucide-react";
 
-const API = "http://localhost:8000";
+const API = "http://159.198.43.22:3000";
 
 const AdminLogin = () => {
   const [username, setUsername] = useState("");
