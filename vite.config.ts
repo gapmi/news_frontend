bdaac68c24168a47a95088f1eaef4fa4828ac5ec
AnatolyM <amisteke@gmail.com>
2026-05-01 17:41:50 +0300

    Revert "+ server c HMR"
    This reverts commit 19102987cf66307bd2e33a58c2a88c60f69de871.

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
-    host: "0.0.0.0",
+    host: "::",
     port: 8080,
-    strictPort: true,
     hmr: {
-      protocol: "wss",
-      host: "aggregator-news.com",
-      clientPort: 443,
+      overlay: false,
     },
+    allowedHosts:['aggregator-news.com']
   },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
-  plugins: [react()],
-});
+}));
